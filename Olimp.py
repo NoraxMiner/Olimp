2701ecbcaabc28e6fc58fa8575c3ad53ba828a14
NoraxMiner <NoraxMiner@tutamail.com>
2024-10-01 23:40:29 +0300

    50%

--- Olimp.py
+++ Olimp.py
@@ -1,22 +1,46 @@
 input_data = open('input.txt', 'r') 
-data= input_data.read() 
+data = input_data.read() 
 #-------------------------------------------------------------------------
+data = data.split()
+a = int(data[0])
+b = int(data[1])
+c = int(data[2])
 
-a = int
-b = int
-c = int
-max = int
-min = int
-if a >b and a > c:
-    max = a
+vpr = 0
+npr = 0
+
+if a > b and a > c:
+    vpr = a
 elif b > a and b > c:
-         
+    vpr = b
+elif c > a and c > b:
+    vpr = c   
+
+
+
+if a < b and a < c:
+    npr = a
+elif b < a and b < c:
+    npr = b
+elif c < a and c < b:
+    npr = c 
+      
+
+vpr = str(vpr)
+npr = str(npr)         
+aut = str(vpr + ' '  + npr)
+
+
+st1 = 0
+st2 = 0
+st3 = 0
+
 
 
 
 #-------------------------------------------------------------------------
 output_data = open('output.txt', 'w') 
-output_data.write(str()) 
+output_data.write(aut)
 
 input_data.close() 
 output_data.close()
