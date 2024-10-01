01876dbece73178a738eed3ce64b620f5283f04b
NoraxMiner <NoraxMiner@tutamail.com>
2024-10-01 16:34:12 +0300

    df

---
+++ Olimp.py
@@ -0,0 +1,22 @@
+input_data = open('input.txt', 'r') 
+data= input_data.read() 
+#-------------------------------------------------------------------------
+
+a = int
+b = int
+c = int
+max = int
+min = int
+if a >b and a > c:
+    max = a
+elif b > a and b > c:
+         
+
+
+
+#-------------------------------------------------------------------------
+output_data = open('output.txt', 'w') 
+output_data.write(str()) 
+
+input_data.close() 
+output_data.close()
