bd7c0aa1c2ba5aa0f72fc30e38f258778f254a0c
NoraxMiner <NoraxMiner@tutamail.com>
2024-10-02 23:23:48 +0300

    -50%

--- Olimp.py
+++ Olimp.py
@@ -8,6 +8,7 @@ c = int(data[2])
 
 vpr = 0
 npr = 0
+sr = 0
 
 if a > b and a > c:
     vpr = a
@@ -15,6 +16,13 @@ elif b > a and b > c:
     vpr = b
 elif c > a and c > b:
     vpr = c   
+elif a == b and a > c: 
+    vpr = a or b     
+elif a == c and a > b: 
+    vpr = a or c 
+elif b == c and b > a: 
+    vpr = b or c        
+
 
 
 
@@ -24,23 +32,65 @@ elif b < a and b < c:
     npr = b
 elif c < a and c < b:
     npr = c 
-      
+elif a == b and a < c: 
+    npr = a or b     
+elif a == c and a < b: 
+    npr = a or c 
+elif b == c and b < a: 
+    npr = b or c        
 
-vpr = str(vpr)
-npr = str(npr)         
-aut = str(vpr + ' '  + npr)
+
+if a > b > c:
+    sr = b
+elif a < b < c:
+    sr = b
+elif b < a < c:
+    sr = a
+elif b > a > c:
+    sr = a
+elif b < c < a:
+    sr = c
+elif b > c > a:
+    sr = c
 
 
-st1 = 0
-st2 = 0
-st3 = 0
+
+
+vpr = int(vpr)
+npr = int(npr)  
+sr = int(sr)        
+
+
+
+
+
+
+st1 = npr + npr + sr + npr + vpr
+st5 = a + a + b + a + b + c 
+st3 = c + c + b + c + b + a
+
+
+aut = 0
+ 
+if st1 < st3 and st1 < st5:
+    aut = 1
+elif st3 < st1 and st3 < st5:
+    aut = 3
+elif st5 < st1 and st5 < st3:
+    aut = 5 
+elif st1 == st5 and st1 < st3: 
+    aut = 1    
+elif st1 == st3 and st1 < st5: 
+    aut = 1 
+elif st3 == st5 and st3 < st1: 
+    aut = 3
 
 
 
 
 #-------------------------------------------------------------------------
 output_data = open('output.txt', 'w') 
-output_data.write(aut)
+output_data.write(str(aut))
 
 input_data.close() 
 output_data.close()
